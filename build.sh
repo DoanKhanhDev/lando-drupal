cb8fbb13bbaba39df85d5d1e1bdf90b4df57d887
DoanKhanh.dev <doankhanh.dev@gmail.com>
2024-03-24 20:31:54 +0700

    refactor code

--- build.sh
+++ build.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-PATH_TO_PROJECT = '/app'
-
-cd PATH_TO_PROJECT
-
-pwd
-
 # Install PHP packages(PHPCS, PHPCBF, Drupal standard).
-echo 'Install php_codesniffer...'
-composer global require "squizlabs/php_codesniffer=*"
-
-echo 'Install Drupal standard...'
-composer global require drupal/coder
+composer global require "squizlabs/php_codesniffer=*" "drupal/coder"
