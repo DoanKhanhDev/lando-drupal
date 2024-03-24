dd7800d86c8a4a9b1315804e1b25c20b43e44409
DoanKhanh.dev <doankhanh.dev@gmail.com>
2024-03-24 20:15:53 +0700

    debug to check installed

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ PATH_TO_PROJECT = '/app'
 
 cd PATH_TO_PROJECT
 
-# Run composer install.
-composer install
+pwd
 
 # Install PHP packages(PHPCS, PHPCBF, Drupal standard).
+echo 'Install php_codesniffer...'
 composer global require "squizlabs/php_codesniffer=*"
 
+echo 'Install Drupal standard...'
 composer global require drupal/coder
